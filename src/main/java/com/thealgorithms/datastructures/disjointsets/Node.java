8f1fb2420cc415f16435974f858eb6d393ad21db
Bahytzhan625 <fenix.bahytzhan@gmail.com>
2021-11-28 15:38:46 +0500

    init

---
+++ src/main/java/com/thealgorithms/datastructures/disjointsets/Node.java
@@ -0,0 +1,13 @@
+package main.java.com.thealgorithms.datastructures.disjointsets;
+
+public class Node<T> {
+
+	public int rank;
+	public Node<T> parent;
+	public T data;
+
+	public Node(T data) {
+		this.data = data;
+		parent = this;
+	}
+}
