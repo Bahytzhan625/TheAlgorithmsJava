ecb5b48b55117e70af3e2b07143c130ce047a394
Bahytzhan625 <fenix.bahytzhan@gmail.com>
2021-12-08 19:39:24 +0500

    init

---
+++ src/main/java/com/thealgorithms/datastructures/heaps/EmptyHeapException.java
@@ -0,0 +1,11 @@
+package main.java.com.thealgorithms.datastructures.heaps;
+
+/**
+ * @author Bahytzhan
+ */
+public class EmptyHeapException extends Exception {
+
+	public EmptyHeapException(String message) {
+		super(message);
+	}
+}
